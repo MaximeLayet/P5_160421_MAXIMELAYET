947e02b58b814607e7de394a103c8854369c44ab
Maxime Layet <layetmaxime@gmail.com>
2021-04-30 14:43:13 +0200

    Début la mise en place du panier

--- JS/confirm.js
+++
@@ -1,5 +0,0 @@
-//Créer un message confirmant l'achat avec remerciement utilisateur, le total et la ref de la commande
-let nomDuClient ;
-let remerciement ;
-let totalTicket ;
-let referenceCommande ;
