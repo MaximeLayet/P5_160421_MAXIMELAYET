bfd8e0e63067bb18db680b2335b9adaeb60e1293
Maxime Layet <layetmaxime@gmail.com>
2021-05-07 16:04:30 +0200

    Index.html et js ok, on continue sur la page product!

--- JS/index.js
+++ JS/index.js
@@ -5,83 +5,37 @@ const url = "http://localhost:3000/api/teddies";
 let product;
 const output = document.getElementById("output");
 
-//Noms//
-
 fetch(url)
 	.then(res => res.json())
 	.then(teddies => {
 		teddies.forEach(teddy => {
+			// Affichage de tous les produits //
+
 			const h2 = document.createElement("h2");
 			h2.textContent = teddy.name;
 			const p = document.createElement("p");
 			p.textContent = teddy.price / 100 + " €";
 			const img = document.createElement("img");
 			img.src = teddy.imageUrl;
+			const h3 = document.createElement("h3");
+			h3.textContent = teddy.description;
 			const a = document.createElement("a");
-			a.textContent = teddy.description;
-			const button = document.createElement("button");
-			button.textContent = "Je te choisis";
+			a.href = `products.html?id=${teddy._id}`;
 
 			output.appendChild(h2);
 			output.appendChild(img);
-			output.appendChild(a);
+			output.appendChild(h3);
 			output.appendChild(p);
-			output.appendChild(button);
+			output.appendChild(a);
+			a.appendChild(img);
 		});
-	});
+	})
+
+	//Afficher un message d'erreur
+	.catch(err => "Oups!");
 
 //Styles//
 
 output.style.fontFamily = "Helvetica";
-output.style.padding = "0.5rem";
+output.style.padding = "0.5rem lg-4";
 output.style.marginBottom = "1rem";
-
-// .......... //
-
-// const teddies = [
-// 	{
-// 		colors: ["Tan", "Chocolate", "Black", "White"],
-// 		_id: "5be9c8541c9d440000665243",
-// 		name: "Norbert",
-// 		price: 2900,
-// 		imageUrl: "http://localhost:3000/images/teddy_1.jpg",
-// 		description:
-// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-// 	},
-// 	{
-// 		colors: ["Pale brown", "Dark brown", "White"],
-// 		_id: "5beaa8bf1c9d440000a57d94",
-// 		name: "Arnold",
-// 		price: 3900,
-// 		imageUrl: "http://localhost:3000/images/teddy_2.jpg",
-// 		description:
-// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-// 	},
-// 	{
-// 		colors: ["Brown"],
-// 		_id: "5beaaa8f1c9d440000a57d95",
-// 		name: "Lenny and Carl",
-// 		price: 5900,
-// 		description:
-// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-// 		imageUrl: "http://localhost:3000/images/teddy_3.jpg"
-// 	},
-// 	{
-// 		colors: ["Brown", "Blue", "Pink"],
-// 		_id: "5beaabe91c9d440000a57d96",
-// 		name: "Gustav",
-// 		price: 4500,
-// 		imageUrl: "http://localhost:3000/images/teddy_4.jpg",
-// 		description:
-// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-// 	},
-// 	{
-// 		colors: ["Beige", "Tan", "Chocolate"],
-// 		_id: "5beaacd41c9d440000a57d97",
-// 		name: "Garfunkel",
-// 		price: 5500,
-// 		description:
-// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-// 		imageUrl: "http://localhost:3000/images/teddy_5.jpg"
-// 	}
-// ];
--- JS/product.js
+++ JS/product.js
@@ -1,14 +1,53 @@
-// Fonction pour afficher le nombre de choix qu'il faut car pas le même nombre de choix par produit
+//Récupérer la photo avec _id de la page précédente//
+
+// const img = document.createElement("img");
+// let search = "_id";
+// img.src = window.location.search;
+// console.log(123);
+
+//Créer les éléments pour y mettre les données//
+// const personalisation = document.getElementById("personalisation");
+// personalisation.appendChild("img");
+
+// Fonction pour afficher le nombre de choix qu'il faut car pas le même nombre de choix par produit//
 
 function chooseColor(color) {
-	let choice = [];
-	for (let i = 0; i < color.lenght; i++) {
-		choice += `<option value= "${i}">${color[i]}</option>`;
+	let choices = "";
+	for (let i = 0; i < color.length; i++) {
+		choices += `<option value= "${i}">${color[i]}</option>`;
 	}
-	return choice;
+	console.log();
+	return choices;
 }
-  
 
 //recupérer l'ID du produit choisi sur index.html
+const personalisation = document.getElementById("personalisation");
+let product = new URL(window.location.href);
+let id = product.searchParams.get("id");
 
-const id = ;
+//appel de l'API
+const fetchTeddy = async id => {
+	return await fetch(`http://localhost:3000/api/teddies/${id}`).then(res => res.json());
+};
+
+// affichage d'un seul produit par page
+const showTeddy = async id => {
+	let teddy = await fetchTeddy(id);
+	console.log(teddy);
+	const img = document.createElement("img");
+	img.src = teddy.imageUrl;
+
+	personalisation.appendChild(img);
+};
+
+showTeddy(id);
+
+// fetch("http://localhost:3000/api/teddies")
+// 	.then(res => res.json())
+// 	.then(teddies => {
+// 		const img = document.createElement("img");
+// 		img.src = teddies.imageUrl;
+
+// 		personalisation.appendChild(img);
+// 	})
+// 	.then(console.log(123));
